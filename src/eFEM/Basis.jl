a01e686c103cedb067644be711edfbbb6b3cf476
Patrick Eastham <patrick.eastham15@gmail.com>
2019-01-13 21:26:55 -0500

    cleaned up for v0.6. haven't yet touched 1.0

--- src/eFEM/Basis.jl
+++ src/eFEM/Basis.jl
@@ -1,62 +1,67 @@
-# File loaded into eFEM
+# File loaded into eFEMpart
 
 """
-  ~~~  EFFICIENT ~~~
+  shape2D(s::Real,t::Real,order::Int)
 
-  shape2D(s::Float64,t::Float64,order::Int)
-
-Computes shape functions of order order (e.g. 1=linear, 2=quadratic)
+Computes shape functions of linear or quadratic order on a quadrilateral element
 
 ASSUMPTIONS:
-- Only available for quadratic meshes, and linear order (order=1)
+- Only available for quadratic elements
+- Assumes computing with double-precision arithmetic
 
 INPUT:
-  s:      [Float64]
+  s:      [Real]
              horizontal location in computation domain [-1,1]^2
-  t:      [Float64]
-             vertical location in computational domain [-1,1]^2
+  t:      [Real]
+             vertical location in computation domain [-1,1]^2
   order:  [Int]
-             Order of basis function (linear or quadratic)
+             Order of basis function (1 -> linear, 2-> quadratic)
 
 OUTPUT:
-  phi:    [Vector{Float64}]
+  phi:    [Vector{Real}]
              Nodal basis functions evaluated at (s,t)
-  dphids  [Vector{Float64}]
+  dphids  [Vector{Real}]
              Derivative in s-direction of nodal basis function
                evaluated at (s,t)
-  dphidt:  [Vector{Float64}]
+  dphidt:  [Vector{Real}]
              Derivative in t-direction of nodal basis function
                evaluated at (s,t)
 """
-function shape2D(s::Float64,t::Float64,order::Int)
+function shape2D(s::T,t::T,order::Int) where T<:Real
+  ein = one(s)
+  tt = typeof(s)
+
+  # linear element
   if order == 1
-    phi    = Array{Float64}(4)
-    dphids = Array{Float64}(4)
-    dphidt = Array{Float64}(4)
+    phi    = Array{tt}(4)
+    dphids = Array{tt}(4)
+    dphidt = Array{tt}(4)
 
-    phi[1]    = 0.25*(1-s)*(1-t)
-    phi[2]    = 0.25*(1+s)*(1-t)
-    phi[3]    = 0.25*(1+s)*(1+t)
-    phi[4]    = 0.25*(1-s)*(1+t)
-    dphids[1] = -0.25*(1-t)
-    dphids[2] =  0.25*(1-t)
-    dphids[3] =  0.25*(1+t)
-    dphids[4] = -0.25*(1+t)
-    dphidt[1] = -0.25*(1-s)
-    dphidt[2] = -0.25*(1+s)
-    dphidt[3] =  0.25*(1+s)
-    dphidt[4] =  0.25*(1-s)
+    phi[1]    = 0.25*(ein-s)*(ein-t)
+    phi[2]    = 0.25*(ein+s)*(ein-t)
+    phi[3]    = 0.25*(ein+s)*(ein+t)
+    phi[4]    = 0.25*(ein-s)*(ein+t)
+    dphids[1] = -0.25*(ein-t)
+    dphids[2] =  0.25*(ein-t)
+    dphids[3] =  0.25*(ein+t)
+    dphids[4] = -0.25*(ein+t)
+    dphidt[1] = -0.25*(ein-s)
+    dphidt[2] = -0.25*(ein+s)
+    dphidt[3] =  0.25*(ein+s)
+    dphidt[4] =  0.25*(ein-s)
+  
+  # quadratic element
   elseif order == 2
-    phi    = Array{Float64}(9)
-    dphids = Array{Float64}(9)
-    dphidt = Array{Float64}(9)
+    phi    = Array{tt}(9)
+    dphids = Array{tt}(9)
+    dphidt = Array{tt}(9)
 
-    ells1  = 0.5*s*(s-1); ellt1 = 0.5*t*(t-1);
-    ells2  = 1-(s*s);     ellt2 = 1-(t*t);
-    ells3  = 0.5*s*(s+1); ellt3 = 0.5*t*(t+1);
-    dells1 = s-0.5;       dellt1 = t-0.5;
-    dells2 = -2*s;        dellt2 = -2*t;
-    dells3 = s+0.5;       dellt3 = t+0.5;
+    ells1  = 0.5*s*(s-ein); ellt1 = 0.5*t*(t-ein);
+    ells2  = ein-(s*s);     ellt2 = ein-(t*t);
+    ells3  = 0.5*s*(s+ein); ellt3 = 0.5*t*(t+ein);
+    dells1 = s-0.5;         dellt1 = t-0.5;
+    dells2 = -2*s;          dellt2 = -2*t;
+    dells3 = s+0.5;         dellt3 = t+0.5;
 
     phi[1] = ells1*ellt1
     phi[2] = ells3*ellt1
@@ -91,31 +96,37 @@ function shape2D(s::Float64,t::Float64,order::Int)
 end
 
 """
-  ~~~  EFFICIENT ~~~
-"""
-function shape2D!(phi,dphids,dphidt,
-                 s::Float64,t::Float64,order::Int)
-  if order == 1
-    phi[1]    = 0.25*(1-s)*(1-t)
-    phi[2]    = 0.25*(1+s)*(1-t)
-    phi[3]    = 0.25*(1+s)*(1+t)
-    phi[4]    = 0.25*(1-s)*(1+t)
-    dphids[1] = -0.25*(1-t)
-    dphids[2] =  0.25*(1-t)
-    dphids[3] =  0.25*(1+t)
-    dphids[4] = -0.25*(1+t)
-    dphidt[1] = -0.25*(1-s)
-    dphidt[2] = -0.25*(1+s)
-    dphidt[3] =  0.25*(1+s)
-    dphidt[4] =  0.25*(1-s)
-  elseif order == 2
+  shape2D!(phi::Vector{T},dphids::Vector{T},dphidt::Vector{T},
+           s::T,t::T,order::Int) where T<:Real
 
-    ells1  = 0.5*s*(s-1);  ellt1 = 0.5*t*(t-1);
-    ells2  = 1-(s*s);      ellt2 = 1-(t*t);
-    ells3  = 0.5*s*(s+1);  ellt3 = 0.5*t*(t+1);
-    dells1 = s-0.5;       dellt1 = t-0.5;
-    dells2 = -2*s;        dellt2 = -2*t;
-    dells3 = s+0.5;       dellt3 = t+0.5;
+in-place version of shape2D; see shape2D function info.
+"""
+function shape2D!(phi,dphids,dphidt,s::T,t::T,order::Int) where T<:Real
+  ein = one(s)
+
+  # linear element
+  if order == 1
+    phi[1]    = 0.25*(ein-s)*(ein-t)
+    phi[2]    = 0.25*(ein+s)*(ein-t)
+    phi[3]    = 0.25*(ein+s)*(ein+t)
+    phi[4]    = 0.25*(ein-s)*(ein+t)
+    dphids[1] = -0.25*(ein-t)
+    dphids[2] =  0.25*(ein-t)
+    dphids[3] =  0.25*(ein+t)
+    dphids[4] = -0.25*(ein+t)
+    dphidt[1] = -0.25*(ein-s)
+    dphidt[2] = -0.25*(ein+s)
+    dphidt[3] =  0.25*(ein+s)
+    dphidt[4] =  0.25*(ein-s)
+
+  # quadratic element
+  elseif order == 2
+    ells1  = 0.5*s*(s-ein);  ellt1 = 0.5*t*(t-ein);
+    ells2  = ein-(s*s);      ellt2 = ein-(t*t);
+    ells3  = 0.5*s*(s+ein);  ellt3 = 0.5*t*(t+ein);
+    dells1 = s-0.5;          dellt1 = t-0.5;
+    dells2 = -2*s;           dellt2 = -2*t;
+    dells3 = s+0.5;          dellt3 = t+0.5;
 
     phi[1] = ells1*ellt1
     phi[2] = ells3*ellt1
@@ -149,77 +160,78 @@ function shape2D!(phi,dphids,dphidt,
   nothing
 end
 
+#"""
+#  ~~~  EFFICIENT ~~~
+#"""
+#function shape2D!(phi,dphids,dphidt,s::Float64,t::Float64,order::Int,gpt::Int)
+#  if order == 1
+#    phi[1,gpt]    = 0.25*(1-s)*(1-t)
+#    phi[2,gpt]    = 0.25*(1+s)*(1-t)
+#    phi[3,gpt]    = 0.25*(1+s)*(1+t)
+#    phi[4,gpt]    = 0.25*(1-s)*(1+t)
+#    dphids[1,gpt] = -0.25*(1-t)
+#    dphids[2,gpt] =  0.25*(1-t)
+#    dphids[3,gpt] =  0.25*(1+t)
+#    dphids[4,gpt] = -0.25*(1+t)
+#    dphidt[1,gpt] = -0.25*(1-s)
+#    dphidt[2,gpt] = -0.25*(1+s)
+#    dphidt[3,gpt] =  0.25*(1+s)
+#    dphidt[4,gpt] =  0.25*(1-s)
+#  elseif order == 2
+#    ells1  = 0.5*s*(s-1);  ellt1 = 0.5*t*(t-1);
+#    ells2  = 1-(s*s);      ellt2 = 1-(t*t);
+#    ells3  = 0.5*s*(s+1);  ellt3 = 0.5*t*(t+1);
+#    dells1 = s-0.5;       dellt1 = t-0.5;
+#    dells2 = -2*s;        dellt2 = -2*t;
+#    dells3 = s+0.5;       dellt3 = t+0.5;
+#
+#    phi[1,gpt] = ells1*ellt1
+#    phi[2,gpt] = ells3*ellt1
+#    phi[3,gpt] = ells3*ellt3
+#    phi[4,gpt] = ells1*ellt3
+#    phi[5,gpt] = ells2*ellt1
+#    phi[6,gpt] = ells3*ellt2
+#    phi[7,gpt] = ells2*ellt3
+#    phi[8,gpt] = ells1*ellt2
+#    phi[9,gpt] = ells2*ellt2
+#    dphids[1,gpt] = dells1*ellt1
+#    dphids[2,gpt] = dells3*ellt1
+#    dphids[3,gpt] = dells3*ellt3
+#    dphids[4,gpt] = dells1*ellt3
+#    dphids[5,gpt] = dells2*ellt1
+#    dphids[6,gpt] = dells3*ellt2
+#    dphids[7,gpt] = dells2*ellt3
+#    dphids[8,gpt] = dells1*ellt2
+#    dphids[9,gpt] = dells2*ellt2
+#    dphidt[1,gpt] = ells1*dellt1
+#    dphidt[2,gpt] = ells3*dellt1
+#    dphidt[3,gpt] = ells3*dellt3
+#    dphidt[4,gpt] = ells1*dellt3
+#    dphidt[5,gpt] = ells2*dellt1
+#    dphidt[6,gpt] = ells3*dellt2
+#    dphidt[7,gpt] = ells2*dellt3
+#    dphidt[8,gpt] = ells1*dellt2
+#    dphidt[9,gpt] = ells2*dellt2
+#  end
+#
+#  nothing
+#end
+
 """
   ~~~  EFFICIENT ~~~
-"""
-function shape2D!(phi,dphids,dphidt,s::Float64,t::Float64,order::Int,gpt::Int)
-  if order == 1
-    phi[1,gpt]    = 0.25*(1-s)*(1-t)
-    phi[2,gpt]    = 0.25*(1+s)*(1-t)
-    phi[3,gpt]    = 0.25*(1+s)*(1+t)
-    phi[4,gpt]    = 0.25*(1-s)*(1+t)
-    dphids[1,gpt] = -0.25*(1-t)
-    dphids[2,gpt] =  0.25*(1-t)
-    dphids[3,gpt] =  0.25*(1+t)
-    dphids[4,gpt] = -0.25*(1+t)
-    dphidt[1,gpt] = -0.25*(1-s)
-    dphidt[2,gpt] = -0.25*(1+s)
-    dphidt[3,gpt] =  0.25*(1+s)
-    dphidt[4,gpt] =  0.25*(1-s)
-  elseif order == 2
-    ells1  = 0.5*s*(s-1);  ellt1 = 0.5*t*(t-1);
-    ells2  = 1-(s*s);      ellt2 = 1-(t*t);
-    ells3  = 0.5*s*(s+1);  ellt3 = 0.5*t*(t+1);
-    dells1 = s-0.5;       dellt1 = t-0.5;
-    dells2 = -2*s;        dellt2 = -2*t;
-    dells3 = s+0.5;       dellt3 = t+0.5;
+  shapeEval(coef::Vector{Real},basis::Vector{Real})
 
-    phi[1,gpt] = ells1*ellt1
-    phi[2,gpt] = ells3*ellt1
-    phi[3,gpt] = ells3*ellt3
-    phi[4,gpt] = ells1*ellt3
-    phi[5,gpt] = ells2*ellt1
-    phi[6,gpt] = ells3*ellt2
-    phi[7,gpt] = ells2*ellt3
-    phi[8,gpt] = ells1*ellt2
-    phi[9,gpt] = ells2*ellt2
-    dphids[1,gpt] = dells1*ellt1
-    dphids[2,gpt] = dells3*ellt1
-    dphids[3,gpt] = dells3*ellt3
-    dphids[4,gpt] = dells1*ellt3
-    dphids[5,gpt] = dells2*ellt1
-    dphids[6,gpt] = dells3*ellt2
-    dphids[7,gpt] = dells2*ellt3
-    dphids[8,gpt] = dells1*ellt2
-    dphids[9,gpt] = dells2*ellt2
-    dphidt[1,gpt] = ells1*dellt1
-    dphidt[2,gpt] = ells3*dellt1
-    dphidt[3,gpt] = ells3*dellt3
-    dphidt[4,gpt] = ells1*dellt3
-    dphidt[5,gpt] = ells2*dellt1
-    dphidt[6,gpt] = ells3*dellt2
-    dphidt[7,gpt] = ells2*dellt3
-    dphidt[8,gpt] = ells1*dellt2
-    dphidt[9,gpt] = ells2*dellt2
-  end
-
-  nothing
-end
-
-"""
-  ~~~  EFFICIENT ~~~
-  shapeEval(coef::Vector{FLoat64},basis::Vector{Float64})
-
-Evaluates the sum given by the basis values and coefficients. Most useful for evaluating functions interior to finite elements
+Evaluates the sum given by the basis values and coefficients. Most useful for 
+evaluating functions interior to finite elements when (s,t) are known
 
 INPUT:
-  coef:    [Vector{Float64}]
+  coef:    [Vector{Real}]
               Coefficients for some quantity evaluated at some point
-  basis:   [Vector{Float64}]
+  basis:   [Vector{Real}]
               Nodal Basis functions evaluated at some point
 
 OUTPUT:
-  interp:  [Float64]
+  interp:  [Real]
               Interpolated value
 """
 function shapeEval(coef::Vector{T},basis::Vector{T}) where T<:Real
@@ -238,31 +250,31 @@ end
 Evaluates derivatives of shape functions
 
 INPUT:
-  s:       [Float64]
+  s:       [Real]
               Horizontal component of point in computational domain [-1,1]^2
-  t:       [Float64]
+  t:       [Real]
               Vertical component of point in computational domain [-1,1]^2
-  xNodes:  [Vector{Float64}]
+  xNodes:  [Vector{Real}]
               Horizontal component of element nodes
-  yNodes:  [Vector{Float64}]
+  yNodes:  [Vector{Real}]
               Vertical component of element nodes
   order:   [Int]
               Order of basis polynomials on element
 
 OUTPUT:
-  phi:     [Vector{Float64}]
+  phi:     [Vector{Real}]
               Nodal basis function evaluated at node (s,t)
-  dphidx:  [Vector{Float64}]
+  dphidx:  [Vector{Real}]
               Derivative in x-direction of nodal basis function
                 evaluated at (s,t)
-  dphidy:  [Vector{Float64}]
+  dphidy:  [Vector{Real}]
               Derivative in y-direction of nodal basis function
                 evaluated at (s,t)
-  jac:     [Float64]
+  jac:     [Real]
               Jacobian of transformation from computation to physical
                 domain (or is it the other way around...?)
 """
-function derivShape2D(s::Float64,t::Float64,xNodes,yNodes,order::Int)
+function derivShape2D(s::T,t::T,xNodes::Vector{T},yNodes::Vector{T},order::Int) where T<:Real
   if order==1
     numNodes=4
   elseif order==2
@@ -299,7 +311,7 @@ function derivShape2D(s::Float64,t::Float64,xNodes,yNodes,order::Int)
 end
 
 """
-  Evaluates dphidx and dphidy by reference but ALSO return jacobian float
+  Evaluates dphidx and dphidy by reference
 """
 function derivShape2D!(phi,dphidx,dphidy,dphids,dphidt,s,t,xNodes,yNodes,order::Int)
 (order==1 ? numNodes=4 : numNodes=9)
@@ -311,8 +323,8 @@ function derivShape2D!(phi,dphidx,dphidy,dphids,dphidt,s,t,xNodes,yNodes,order::
 
   # evaluate derivatives
   for i=1:numNodes
-  dphidx[i] =  dphids[i]*dydt - dphidt[i]*dyds
-  dphidy[i] = -dphids[i]*dxdt + dphidt[i]*dxds
+    dphidx[i] =  dphids[i]*dydt - dphidt[i]*dyds
+    dphidy[i] = -dphids[i]*dxdt + dphidt[i]*dxds
   end
 
   nothing
@@ -340,8 +352,6 @@ function jacCalc(xNodes,yNodes,dphids,dphidt)
 end
 
 """
-  ~~~  EFFICIENT ~~~
-
   GaussQuadPoints2D(order::Int)
 
 Produces Gauss Quadrature weights and points, normalized to (-1,1), for a given order
@@ -351,18 +361,18 @@ INPUT:
              Order of gauss quadrature
 
 OUTPUT:
-  w:      [Vector{Float64}]
+  w:      [Vector{Real}]
              Weights of Guass Quadrature values
-  s:      [Vector{Float64}]
+  s:      [Vector{Real}]
              Horizontal components of Gauss Quadrature nodes
-  t:      [Vector{Float64}]
+  t:      [Vector{Real}]
              Horizontal components of Gauss Quadrature nodes
 """
-function GaussQuadPoints2D(order::Int)
+function GaussQuadPoints2D(order::Int;tt=Float64::DataType)
   if order==2        # 2x2 Gauss points
-    w = Array{Float64,1}(4)
-    s = Array{Float64,1}(4)
-    t = Array{Float64,1}(4)
+    w = Array{tt,1}(4)
+    s = Array{tt,1}(4)
+    t = Array{tt,1}(4)
 
     gpt=1.0e0/sqrt(3.0e0);
     s[1] = -gpt; t[1] = -gpt; w[1]=1.0;
@@ -370,9 +380,9 @@ function GaussQuadPoints2D(order::Int)
     s[3] =  gpt; t[3] =  gpt; w[3]=1.0;
     s[4] = -gpt; t[4] =  gpt; w[4]=1.0;
   elseif order==3    # 3x3 Gauss points
-    w = zeros(Float64,9)
-    s = zeros(Float64,9)
-    t = zeros(Float64,9)
+    w = zeros(tt,9)
+    s = zeros(tt,9)
+    t = zeros(tt,9)
 
     gpt=sqrt(0.6);
     s[1] = -gpt; t[1] = -gpt; w[1]=25/81
@@ -385,9 +395,9 @@ function GaussQuadPoints2D(order::Int)
     s[8] = -gpt; t[8] =  0.0; w[8]=40/81
     s[9] =  0.0; t[9] =  0.0; w[9]=64/81
   elseif order==4      # 4x4 Gauss points
-    w = zeros(Float64,16)
-    s = zeros(Float64,16)
-    t = zeros(Float64,16)
+    w = zeros(tt,16)
+    s = zeros(tt,16)
+    t = zeros(tt,16)
 
     tmp  = 2/7*sqrt(6/5)
     gpt1 = sqrt(3/7-tmp)
@@ -416,66 +426,6 @@ function GaussQuadPoints2D(order::Int)
   end
 
   return w,s,t
-end
-function GaussQuadPoints2DBigFloat(order::Int)
-  if order==2        # 2x2 Gauss points
-    w = Array{BigFloat,1}(4)
-    s = Array{BigFloat,1}(4)
-    t = Array{BigFloat,1}(4)
-
-    gpt=1.0e0/sqrt(3.0e0);
-    s[1] = -gpt; t[1] = -gpt; w[1]=1.0;
-    s[2] =  gpt; t[2] = -gpt; w[2]=1.0;
-    s[3] =  gpt; t[3] =  gpt; w[3]=1.0;
-    s[4] = -gpt; t[4] =  gpt; w[4]=1.0;
-  elseif order==3    # 3x3 Gauss points
-    w = zeros(BigFloat,9)
-    s = zeros(BigFloat,9)
-    t = zeros(BigFloat,9)
-
-    gpt=BigFloat(sqrt(0.6))
-    s[1] = -gpt;       t[1] = -gpt;        w[1]=BigFloat(25/81)
-    s[2] =  gpt;       t[2] = -gpt;        w[2]=BigFloat(25/81)
-    s[3] =  gpt;       t[3] =  gpt;        w[3]=BigFloat(25/81)
-    s[4] = -gpt;       t[4] =  gpt;        w[4]=BigFloat(25/81)
-    s[5] =  zero(gpt); t[5] = -gpt;        w[5]=BigFloat(40/81)
-    s[6] =  gpt;       t[6] =  zero(gpt);  w[6]=BigFloat(40/81)
-    s[7] =  zero(gpt); t[7] =  gpt;        w[7]=BigFloat(40/81)
-    s[8] = -gpt;       t[8] =  zero(gpt);  w[8]=BigFloat(40/81)
-    s[9] =  zero(gpt); t[9] =  zero(gpt);  w[9]=BigFloat(64/81)
-  elseif order==4      # 4x4 Gauss points
-    w = zeros(BigFloat,16)
-    s = zeros(BigFloat,16)
-    t = zeros(BigFloat,16)
-
-    tmp  = 2/7*sqrt(6/5)
-    gpt1 = sqrt(3/7-tmp)
-    gpt2 = sqrt(3/7+tmp)
-    wp   = (18+sqrt(30))/36
-    wm   = (18-sqrt(30))/36
-    s[1]  =  gpt1; t[1]  =  gpt1; w[1]  = wp*wp
-    s[2]  =  gpt1; t[2]  = -gpt1; w[2]  = wp*wp
-    s[3]  =  gpt1; t[3]  =  gpt2; w[3]  = wp*wm
-    s[4]  =  gpt1; t[4]  = -gpt2; w[4]  = wp*wm
-    s[5]  = -gpt1; t[5]  =  gpt1; w[5]  = wp*wp
-    s[6]  = -gpt1; t[6]  = -gpt1; w[6]  = wp*wp
-    s[7]  = -gpt1; t[7]  =  gpt2; w[7]  = wp*wm
-    s[8]  = -gpt1; t[8]  = -gpt2; w[8]  = wp*wm
-
-    s[9]  =  gpt2; t[9]  =  gpt1; w[9]  = wm*wp
-    s[10] =  gpt2; t[10] = -gpt1; w[10] = wm*wp
-    s[11] =  gpt2; t[11] =  gpt2; w[11] = wm*wm
-    s[12] =  gpt2; t[12] = -gpt2; w[12] = wm*wm
-    s[13] = -gpt2; t[13] =  gpt1; w[13] = wm*wp
-    s[14] = -gpt2; t[14] = -gpt1; w[14] = wm*wp
-    s[15] = -gpt2; t[15] =  gpt2; w[15] = wm*wm
-    s[16] = -gpt2; t[16] = -gpt2; w[16] = wm*wm
-  else
-    error("Check Gauss point integration specification")
-  end
-
-  return w,s,t
-
 end
 
 """
@@ -494,33 +444,33 @@ INPUT:
                   Order of basis interpolating function
 
 OUTPUT:
-  w:           [Vector{Float64}]
+  w:           [Vector{Real}]
                   Weights of Guass Quadrature values
-  s:           [Vector{Float64}]
+  s:           [Vector{Real}]
                   Horizontal components of Gauss Quadrature nodes
-  t:           [Vector{Float64}]
+  t:           [Vector{Real}]
                   Horizontal components of Gauss Quadrature nodes
 """
-function GaussEdge(edgeNumber::Int,order::Int)
-  w,_ = GaussQuadPoints1D(order)
+function GaussEdge(edgeNumber::Int,order::Int;tt=Float64::DataType)
+  w,_ = GaussQuadPoints1D(order;tt=tt)
 
   if edgeNumber == 1
-    _,s = GaussQuadPoints1D(order)
+    _,s = GaussQuadPoints1D(order;tt=tt)
     t   = -ones(order+1)
   elseif edgeNumber==2
     s   = ones(order+1)
-    _,t = GaussQuadPoints1D(order)
+    _,t = GaussQuadPoints1D(order;tt=tt)
   elseif edgeNumber==3
-    _,s = GaussQuadPoints1D(order)
+    _,s = GaussQuadPoints1D(order;tt=tt)
     t   = ones(order+1)
   elseif edgeNumber==4
     s   = -ones(order+1)
-    _,t = GaussQuadPoints1D(order)
+    _,t = GaussQuadPoints1D(order;tt=tt)
   else
     error("Invalid Edge Number entered. Must be 1-4, but $(edgeNumber) was entered")
   end
 
-  return w::Vector{Float64},s::Vector{Float64},t::Vector{Float64}
+  return w::Vector{tt},s::Vector{tt},t::Vector{tt}
 end
 
 
@@ -562,13 +512,13 @@ end
 
 Produces Gauss Quadrature weights and points, normalized to (-1,1), for a given order
 """
-function GaussQuadPoints1D(order::Int)
-	w = zeros(Float64,order)
-	s = zeros(Float64,order)
+function GaussQuadPoints1D(order::Int;tt=Float64::DataType)
+	w = zeros(tt,order)
+	s = zeros(tt,order)
 
 	if order == 2
-		w[1] = 1
-		w[2] = 1
+		w[1] = one(tt)
+		w[2] = one(tt)
 		s[1] = sqrt(1/3)
 		s[2] = -s[1]
   elseif order == 3
